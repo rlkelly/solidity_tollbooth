0d08880988fe11e10949f030647cf2248331cda4
Xavier Lepretre <xavier@b9lab.com>
2018-03-18 16:33:49 +0000

    Fix silly typo

--- contracts/Regulator.sol
+++ contracts/Regulator.sol
@@ -35,7 +35,7 @@ contract Regulator is RegulatorI, Owned {
         public
         fromOwner
         returns(TollBoothOperatorI newOperator) {
-            require(owner != getOwner())
+            require(owner != getOwner());
             TollBoothOperator myOperator = new TollBoothOperator(false, deposit, this);
             approvedOperators.push(myOperator);
             approvedOperatorsMapping[myOperator] = true;
